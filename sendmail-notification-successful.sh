543c4cee7cbe6582df2d3278c67651b1d9246666
Vojtech Myslivec <vojtech.myslivec@fit.cvut.cz>
2016-02-10 00:15:23 +0100

    Added a notification about successful cert renewal

---
+++ sendmail-notification-successful.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+email=""
+sendmail="/opt/zimbra/postfix/sbin/sendmail"
+subject="Certificate renewal in $1 day(s)"
+message="Hello,
+this is just a kindly reminder that a letsencrypt-zimbra tool
+renewed successfully your Zimbra certificate!
+
+Sincerelly yours,
+cron"
+
+USAGE="USAGE
+    $0
+
+    This simple script will send the email to e-mail '$email' via
+    '$sendmail' program."
+
+[ $# -ne 0 ] && {
+    echo "$USAGE" >&2
+    exit 1
+}
+
+echo "Subject: $subject
+
+$message" | "$sendmail" "$email"
+
