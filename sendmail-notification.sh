6e68ea49e04bac9b50d022af28ccc8a163667610
Vojtech Myslivec <vojtech.myslivec@fit.cvut.cz>
2016-02-09 23:56:58 +0100

    Added a mail notification script

---
+++ sendmail-notification.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+email=""
+sendmail="/opt/zimbra/postfix/sbin/sendmail"
+subject="Certificate renewal in $1 day(s)"
+message="Hello,
+this is just a kindly reminder that a letsencrypt-zimbra tool
+will try to obtain and install new zimbra certificate in $1 day(s).
+
+Sincerelly yours,
+cron"
+
+USAGE="USAGE
+    $0 days
+
+    This simple script will send the email to e-mail '$email' via
+    '$sendmail' program. You must to specify the number of days in
+    the message: 'message'."
+
+[ $# -ne 1 ] && {
+    echo "$USAGE" >&2
+    exit 1
+}
+
+[[ "$1" =~ ^[0-9]+$ ]] || {
+    echo "The first arg '$1' must be a natural number" >&2
+    exit 1
+}
+
+
+echo "Subject: $subject
+
+$message" | "$sendmail" "$email"
+
