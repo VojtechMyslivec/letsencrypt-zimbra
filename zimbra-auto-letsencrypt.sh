acd3f636215cefd9c3f8c14497578904e4f274b8
Lorenzo Faleschini <lorenzo.faleschini@nordestsystems.com>
2016-10-17 23:28:26 +0200

    Update zimbra-auto-letsencrypt.sh

--- zimbra-auto-letsencrypt.sh
+++ zimbra-auto-letsencrypt.sh
@@ -253,8 +253,8 @@ start_nginx
 
 su -c "cp -r /opt/zimbra/ssl/zimbra /opt/zimbra/ssl/zimbra.'$(date +%Y%m%d)'" - "$zimbra_user"
 
-cp '$letsencrypt_issued_key_file' /opt/zimbra/ssl/zimbra/commercial/commercial.key
-cp $letsencrypt_issued_fullchain_file "$temp_dir/cert.pem"
+cp "$letsencrypt_issued_key_file" /opt/zimbra/ssl/zimbra/commercial/commercial.key
+cp "$letsencrypt_issued_fullchain_file" "$temp_dir/cert.pem"
 cat "$letsencrypt_issued_chain_file" "$root_CA_file" > "${temp_dir}/zimbra_chain.pem"
 
 chown -R "$zimbra_user":"$zimbra_user" $temp_dir
