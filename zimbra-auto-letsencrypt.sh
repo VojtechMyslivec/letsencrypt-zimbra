a50162212278c1e7d96b2b239244e5a74e2ca5aa
Lorenzo Faleschini <lorenzo.faleschini@nordestsystems.com>
2016-10-17 22:56:46 +0200

    Update zimbra-auto-letsencrypt.sh

--- zimbra-auto-letsencrypt.sh
+++ zimbra-auto-letsencrypt.sh
@@ -45,11 +45,11 @@ USAGE="USAGE
     Crontab example for autorenewal:
 
 # send a notification a week before the certificate will be obtained
-0 0 1 */2 * root /root/zimbra-auto-letsencrypt/sendmail-notification.sh 7 your@email.com >/dev/null 2>&1
+0 0 1 */2 * /root/zimbra-auto-letsencrypt/sendmail-notification.sh 7 your@email.com >/dev/null 2>&1
 # send a notification a day before the certificate will be obtained
-0 0 7 */2 * root /root/zimbra-auto-letsencrypt/sendmail-notification.sh 1 your@email.com >/dev/null 2>&1
+0 0 7 */2 * /root/zimbra-auto-letsencrypt/sendmail-notification.sh 1 your@email.com >/dev/null 2>&1
 # obtain the certificate
-0 0 8 */2 * root /root/zimbra-auto-letsencrypt/zimbra-auto-letsencrypt.sh --renew /root/zimbra-auto-letsencrypt/letsencrypt-zimbra.conf && /root/zimbra-auto-letsencrypt/sendmail-notification-successful.sh your@email.com >/dev/null 2>&1
+0 0 8 */2 * /root/zimbra-auto-letsencrypt/zimbra-auto-letsencrypt.sh --renew /root/zimbra-auto-letsencrypt/letsencrypt-zimbra.conf && /root/zimbra-auto-letsencrypt/sendmail-notification-successful.sh your@email.com >/dev/null 2>&1
 
     Friendly notice: 
     restarting Zimbra service take a while (1+ m).
