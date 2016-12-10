ee62ce1533ae9429b4f5f9a4c7c45fab1c7d2a89
Vojtech Myslivec <vojtech@xmyslivec.cz>
2016-12-10 18:56:07 +0100

    Join sendmail notification scripts

--- sendmail-notification.sh
+++ sendmail-notification.sh
@@ -1,27 +1,70 @@
 #!/bin/bash
+set -o nounset
+
+SCRIPTNAME=${0##*/}
+
 USAGE="USAGE
-    $0 days
+    $SCRIPTNAME -h | --help | help
+    $SCRIPTNAME --success
+    $SCRIPTNAME --reminder days
 
     This simple script will send an e-mail to 'email' address via
-    'sendmail' program. You must to specify the number of days which
-    will be used in the 'reminder_message'.
+    'sendmail' program.
 
-    The 'email', 'sendmail' and 'reminder_message' are specified
-    in letsencrypt-zimbra config file (see main script' help)."
+    It will use 'reminder_message' or 'success_message', depending
+    on the paramter you used. If you call it with --reminder parameter,
+    you must to specify the number of days which will be used in the
+    'reminder_message'.
 
-[ $# -ne 1 ] && {
+    The 'email', 'sendmail', 'reminder_message' and 'success_message'
+    are specified in letsencrypt-zimbra config file (see main script
+    help)."
+
+exit_with_usage() {
     echo "$USAGE" >&2
     exit 1
 }
 
-[[ "$1" =~ ^[0-9]+$ ]] || {
-    echo "The first arg '$1' must be a natural number" >&2
-    exit 1
+# Usage
+[ $# -eq 1 -o $# -eq 2 ] || {
+    exit_with_usage
 }
 
 letsencrypt_zimbra_dir="${0%/*}"
 source "$letsencrypt_zimbra_dir/letsencrypt-zimbra.cfg"
 
-echo "Subject: $reminder_subject
+case "$1" in
+    "-h" | "--help" | "help" )
+        echo "$USAGE"
+        exit 0
+        ;;
 
-$reminder_message" | "$sendmail" "$email"
+    "--success" )
+        [ $# -eq 1 ] || {
+            exit_with_usage
+        }
+        subject=$success_subject
+        message=$success_message
+        ;;
+
+    "--reminder" )
+        [ $# -eq 2 ] || {
+            exit_with_usage
+        }
+        [[ "$2" =~ ^[0-9]+$ ]] || {
+            echo "days '$2' must be a natural number" >&2
+            exit 1
+        }
+        subject=$reminder_subject
+        message=$reminder_message
+        ;;
+
+    * )
+        echo "$USAGE" >&2
+        exit 1
+        ;;
+esac
+
+echo "Subject: $subject
+
+$message" | cat - # "$sendmail" "$email"
