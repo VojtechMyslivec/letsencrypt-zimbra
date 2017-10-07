830ad78608f23468d255276b7820dec23d03e071
Volodymyr Ivaniuta <ivanyuta.vova@gmail.com>
2017-10-07 18:24:07 +0200

    Update sendmail-notification.sh
    changed variables name to load from .cfg file

--- sendmail-notification.sh
+++ sendmail-notification.sh
@@ -19,7 +19,7 @@ USAGE="USAGE
 letsencrypt_zimbra_dir="${0%/*}"
 source "$letsencrypt_zimbra_dir/letsencrypt-zimbra.cfg"
 
-echo "Subject: $subject
+echo "Subject: $subject_reminder
 
-$message" | "$sendmail" "$email"
+$message_reminder" | "$sendmail" "$email"
 
