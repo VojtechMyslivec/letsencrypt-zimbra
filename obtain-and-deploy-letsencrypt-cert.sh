54de685f6abc3a9f5ef0e6105c24e4c180b5dfdf
Vojtech Myslivec <vojtech@xmyslivec.cz>
2019-02-06 23:14:34 +0100

    Fix version number

--- obtain-and-deploy-letsencrypt-cert.sh
+++ obtain-and-deploy-letsencrypt-cert.sh
@@ -42,7 +42,7 @@ OPTIONS
             certificate, but useful for testing"
 
 # script version: major.minor(.patch)
-VERSION='0.3'
+VERSION='0.4.1'
 
 # --------------------------------------------------------------------
 # -- Functions -------------------------------------------------------
