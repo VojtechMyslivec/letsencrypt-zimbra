e858be43283a50dc6d856b18b1ec1a024eef950e
Vojtech Myslivec <vojtech@xmyslivec.cz>
2020-05-01 23:35:44 +0200

    Increase patch version

--- obtain-and-deploy-letsencrypt-cert.sh
+++ obtain-and-deploy-letsencrypt-cert.sh
@@ -42,7 +42,7 @@ OPTIONS
             certificate, but useful for testing"
 
 # script version: major.minor(.patch)
-VERSION='0.5.1'
+VERSION='0.5.2'
 
 # --------------------------------------------------------------------
 # -- Functions -------------------------------------------------------
