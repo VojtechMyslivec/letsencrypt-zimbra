125cb36c014ae7d496381c7fa3215fd3e4f91e2c
Vojtech Myslivec <vojtech@xmyslivec.cz>
2023-06-22 15:47:36 +0200

    Revert "Fix: intermediate certificates are in wrong order"
    This reverts commit efc0b921ee7815eb77cd49b827a7e009480f519f.

--- letsencrypt-zimbra.sh
+++ letsencrypt-zimbra.sh
@@ -466,7 +466,7 @@ readable_file "$intermediate_CA_file" || {
 }
 
 # create one CA chain file
-cat "$intermediate_CA_file" "$root_CA_file" > "$chain_file"
+cat "$root_CA_file" "$intermediate_CA_file" > "$chain_file"
 
 # verify it with Zimbra tool
 information "test and deploy certificates"
